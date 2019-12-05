0359dfdacb0eedf70afa064720f33f16e26797cf
msl47 <msl47@uclive.ac.nz>
2019-12-05 22:43:50 +1300

    - Added 2 new projects - Added Scroll to top button - Some description changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,29 @@ jQuery(document).ready(function($) {
     for (var i = 0; i < imgs.length; i++) {
         imgs[i].addEventListener('click', expandImg, false);
     }
- 
-    
+
+    //Get the button:
+    mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.onclick = function topFunction() {
+        window.scroll({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth'
+        });
+    }
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        } 
+        else {
+            mybutton.style.display = "none";
+        }
+    }
+
 });
