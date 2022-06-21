8b0b46f6f769d6a5cc0166852fc4dbee492e0843
Mike Lee <mike.lee@untamedisles.com>
2022-06-21 14:51:25 +1200

    New portfolio layout. Added new games and removed some old ones and removed unnecessary sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,6 @@
 jQuery(document).ready(function($) {
-
-    var imgs = document.getElementsByClassName("armaImgs");
-    
-    var expandImg = function() {
-        var modal = document.getElementById("myModal");
-        var modalImg = document.getElementById("expandedImg");
-        var captionText = document.getElementById("caption");
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-        modal.style.display = "block";
-        
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-    
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() { 
-        modal.style.display = "none";
-        }
-    }
-
-    for (var i = 0; i < imgs.length; i++) {
-        imgs[i].addEventListener('click', expandImg, false);
-    }
-
     //Get the button:
-    mybutton = document.getElementById("myBtn");
+    mybutton = document.getElementById("TopBtn");
 
     // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
