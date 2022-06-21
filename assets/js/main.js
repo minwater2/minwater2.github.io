19b8b29cdc617ff674a330932a40fb3c86fbd9c1
Mike Lee <mike.lee@untamedisles.com>
2022-06-21 15:24:04 +1200

    made layout more flexible to mobile device

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
 jQuery(document).ready(function($) {
+
+    var imgs = document.getElementsByClassName("expandableImgs");
+    
+    var expandImg = function() {
+        var modal = document.getElementById("myModal");
+        var modalImg = document.getElementById("expandedImg");
+        var captionText = document.getElementById("caption");
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        modal.style.display = "block";
+        
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+    
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() { 
+        modal.style.display = "none";
+        }
+    }
+
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].addEventListener('click', expandImg, false);
+    }
+
     //Get the button:
     mybutton = document.getElementById("TopBtn");
 
